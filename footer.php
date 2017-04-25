ec1272972be6948f52e20b114faf30804e4070e7
Tom Myruski <thomas.myruski@gmail.com>
2017-04-25 17:01:21 -0400

    Added D4GP Spinning Logo… Also got rid of old javascript calls from long ago that don’t even work.  I will replace these image paths with relative ones when miko supplies the correct graphics

--- footer.php
+++ footer.php
@@ -14,37 +14,10 @@
 
 <div id="footer">
   <div class="container">
-    <p>Copyright &copy; <?php echo get_bloginfo('name') ; ?>. Designed by <a href="http://www.d4gp.com" rel="nofollow" target="_blank">D4GP</a></p>
+     Copyright &copy; <?php echo get_bloginfo('name') ; ?>. <a href="http://www.d4gp.com" target="blank" class="d4gp-link"><div id="f1_container"><div id="f1_card"><div class="front face"><img src="http://www.stevewaitt.com/wp-content/uploads/2016/06/D4GP_1.png" width="47" height="auto" alt="Design For Good People"></div><div class="back face center"><img src="http://www.stevewaitt.com/wp-content/uploads/2016/06/D4GP_2.png" width="47" height="auto" alt="Design For Good People"></div></div></div></a>
   </div>
 </div>
 </div><!--page-content-->
-
-
-
-
-
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -
-<script type="text/javascript" src="js/jquery.1.11.1.js"></script> -->
-<!-- Include all compiled plugins (below), or include individual files as needed 
-<script type="text/javascript" src="<?php get_template_directory_uri() ?>js/bootstrap.js"></script> 
-<script type="text/javascript" src="<?php get_template_directory_uri() ?>js/SmoothScroll.js"></script> 
-<script type="text/javascript" src="<?php get_template_directory_uri() ?>js/jquery.prettyPhoto.js"></script> 
-<script type="text/javascript" src="<?php get_template_directory_uri() ?>js/jquery.parallax.js"></script> 
-<script type="text/javascript" src="<?php get_template_directory_uri() ?>js/jqBootstrapValidation.js"></script> 
-<script type="text/javascript" src="<?php get_template_directory_uri() ?>js/contact_me.js"></script> 
-   <script src="<?php get_template_directory_uri() ?>js/jquery.easing.js"></script>
-
-  <script src="<?php get_template_directory_uri() ?>js/jquery.mousewheel.js"></script>
-
-  <script defer src="<?php get_template_directory_uri() ?>js/demo.js"></script>-->
-
-
-
-<!-- Javascripts 
-    ==================================================
-<script type="text/javascript" src="<?php get_template_directory_uri() ?>js/main.js"></script> -->
-
-
  
 <?php wp_footer(); ?>
 
