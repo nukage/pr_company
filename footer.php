4a9fa5d1bd62ef8f1e33b8cb363e06c975436f0b
Tom Myruski <thomas.myruski@gmail.com>
2017-05-05 16:26:33 -0400

    footer-widget php

--- footer.php
+++ footer.php
@@ -14,9 +14,30 @@
 
 <div id="footer">
   <div class="container">
-    <div id="twitter-widget">
-    <?php dynamic_sidebar( 'footer-sidebar' ); ?>
+
+    
+    <div class="col-sm-4 footer-contacts">
+      <p>Kip Kouri /    <span>Principal</span><br><a href="mailto:kip@tellallyourfriendspr.com">kip[at]tellallyourfriendspr[dot]com</a></p>
+      <p>Molly Small /  <span>Marketing &amp; Special Events</span> <br><a href="mailto:molly@tellallyourfriendspr.com">molly[at]tellallyourfriendspr[dot]com</a></p>
+      <p>Hector Montes / <span>&nbsp;Radio</span><br><a href="mailto:hector@tellallyourfriendspr.com">hector[at]tellallyourfriendspr[dot]com</a></p>
+      <p>Trevor Morrison / <span>&nbsp;Radio</span><br><a href="http://trevor[at]tellallyourfriendspr[dot]com">trevor[at]tellallyourfriendspr[dot]com</a></p>
+      <p>o/ 212.226.3792<br>f/ 212.226.2373</p>
+
+
     </div>
+    <div class="col-sm-4 footer-contacts">
+      <p>Leslie Cuc / <span> Publicist</span> <br><a href="mailto:leslie@tellallyourfriendspr.com">leslie[at]tellallyourfriendspr[dot]com</a></p>
+      <p>Wendy Waseige &nbsp;/ <span> Publicist</span> <br><a href="http://wendy[at]tellallyourfriendspr[dot]com">wendy[at]tellallyourfriendspr[dot]com</a></p>
+      <p>Kate Rakvic / <span> Publicist</span> <br><a href="http://kate[at]tellallyourfirendspr[dot]com">kate[at]tellallyourfirendspr[dot]com</a></p>
+<p>Michael Lantz / <span>&nbsp;Publicist</span>&nbsp;<br><a href="http://michael[at]tellallyourfriendspr[dot]com">michael[at]tellallyourfriendspr[dot]com</a></p>
+
+
+
+    </div>
+    <div class="col-sm-4"> <div id="twitter-widget"> <?php dynamic_sidebar( 'footer-sidebar' ); ?>
+    </div></div>
+   
+   
      Copyright &copy; <?php echo get_bloginfo('name') ; ?>. <a href="http://www.d4gp.com" target="blank" class="d4gp-link">
      <div id="f1_container">
      	<div id="f1_card">
--- template-parts/footer-widget.php
+++ template-parts/footer-widget.php
@@ -1,1 +1,20 @@
-<a class="twitter-timeline" data-width="220" data-height="250" data-theme="dark" data-link-color="#A66AD6" href="https://twitter.com/TAYFPR">Tweets by TAYFPR</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+<div class="col-sm-4 footer-contacts">
+      <p>Kip Kouri /    <span>Principal</span><br><a href="mailto:kip@tellallyourfriendspr.com">kip[at]tellallyourfriendspr[dot]com</a></p>
+      <p>Molly Small /  <span>Marketing &amp; Special Events</span> <br><a href="mailto:molly@tellallyourfriendspr.com">molly[at]tellallyourfriendspr[dot]com</a></p>
+      <p>Hector Montes / <span>&nbsp;Radio</span><br><a href="mailto:hector@tellallyourfriendspr.com">hector[at]tellallyourfriendspr[dot]com</a></p>
+      <p>Trevor Morrison / <span>&nbsp;Radio</span><br><a href="http://trevor[at]tellallyourfriendspr[dot]com">trevor[at]tellallyourfriendspr[dot]com</a></p>
+      <p>o/ 212.226.3792<br>f/ 212.226.2373</p>
+
+
+    </div>
+    <div class="col-sm-4 footer-contacts">
+      <p>Leslie Cuc / <span> Publicist</span> <br><a href="mailto:leslie@tellallyourfriendspr.com">leslie[at]tellallyourfriendspr[dot]com</a></p>
+      <p>Wendy Waseige &nbsp;/ <span> Publicist</span> <br><a href="http://wendy[at]tellallyourfriendspr[dot]com">wendy[at]tellallyourfriendspr[dot]com</a></p>
+      <p>Kate Rakvic / <span> Publicist</span> <br><a href="http://kate[at]tellallyourfirendspr[dot]com">kate[at]tellallyourfirendspr[dot]com</a></p>
+<p>Michael Lantz / <span>&nbsp;Publicist</span>&nbsp;<br><a href="http://michael[at]tellallyourfriendspr[dot]com">michael[at]tellallyourfriendspr[dot]com</a></p>
+
+
+
+    </div>
+    <div class="col-sm-4"> <div id="twitter-widget"> <?php dynamic_sidebar( 'footer-sidebar' ); ?>
+    </div></div>
