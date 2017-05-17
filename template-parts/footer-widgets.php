7a32542f0a7eefcf7c1aee49c2aff03288351f0b
Tom Myruski <thomas.myruski@gmail.com>
2017-05-17 15:45:20 -0400

    Removed ID info in footer

--- template-parts/footer-widgets.php
+++ template-parts/footer-widgets.php
@@ -1,15 +1,5 @@
     <div class="col-sm-4 footer-contacts">
-       <!-- <p>Kip Kouri /    <span>Principal</span><br><a href="mailto:kip@tellallyourfriendspr.com">kip[at]tellallyourfriendspr[dot]com</a></p>
-        <p>Molly Small /  <span>Marketing &amp; Special Events</span> <br><a href="mailto:molly@tellallyourfriendspr.com">molly[at]tellallyourfriendspr[dot]com</a></p>
-        <p>Hector Montes / <span>&nbsp;Radio</span><br><a href="mailto:hector@tellallyourfriendspr.com">hector[at]tellallyourfriendspr[dot]com</a></p>
-        <p>Trevor Morrison / <span>&nbsp;Radio</span><br><a href="http://trevor[at]tellallyourfriendspr[dot]com">trevor[at]tellallyourfriendspr[dot]com</a></p>
-        <p>o/ 212.226.3792<br>f/ 212.226.2373</p>-->
-
-
-        <p>MANHATTAN OFFICE:<br>
-        <a href="tel:(212) 226-3792" target="_blank">(212) 226-3792</a></p>
-        <p>BROOKLYN OFFICE:<br>
-        <a href="#" target="_blank">(555) 224-2525</a></p>
+       
     </div>
 
     <div class="col-sm-4 footer-contacts">
