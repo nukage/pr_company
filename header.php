9dfb965a3c5e72d68a2068083dedbaeaf7e31660
Tom Myruski <thomas.myruski@gmail.com>
2017-11-30 14:59:43 -0500

    Fixed header looking for CSS not there

--- header.php
+++ header.php
@@ -19,17 +19,17 @@
 <meta name="description" content="">
 <meta name="author" content="">
 
-<!-- Bootstrap -->
+<!-- Bootstrap 
 <link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
 
 <!-- Stylesheet
-    ================================================== -->
+    ================================================== 
 <link rel="stylesheet" type="text/css"  href="css/flexslider.css">
 <link rel="stylesheet" type="text/css"  href="css/style.css">
 
  
-<link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
+<link rel="stylesheet" type="text/css" href="css/prettyPhoto.css"> -->
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300' rel='stylesheet' type='text/css'>
 <!--
 <script type="text/javascript" src="js/modernizr.custom.js"></script>
